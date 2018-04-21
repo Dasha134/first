82829b3a4a621e5ecb6559360c0bb2666f866282
Darja <dasha_134@yahoo.ie>
2018-04-21 23:14:44 +0300

    multiplication

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main (void)
 { 
-	int a, b, c, d;
+	int a, b, c, d, e;
 	printf("Input two numbera : ");
 	scanf("%d%d", &a, &b);
 	a+b=c;
 	a-b=d;
+	a*b=e;
 	printf("Sum = %d", c);
 	printf("Differencce is : %d", d);
+	printf("Multiplication = %d", e);
 	return 0;
 }
